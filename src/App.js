cd0466bcc90422f055b3a7dcc2cb90bb52fb0ec7
sandeepdhacse <dhacse.sandeep0@gmail.com>
2022-09-21 23:00:24 +0530

    this is react app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>This is a my first react app</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
